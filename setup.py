e1aa95e79c6fb5472a16291caf6fe4726aa14514
Jan Dvořák <mordae@anilinux.org>
2014-01-27 10:59:16 +0100

    split celly from shared repository
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'celly',
+    version = '1',
+    author = 'The PonyCloud Team',
+    description = ('Python client for the Sparkle API'),
+    license = 'MIT',
+    keywords = 'cloud management',
+    url = 'http://github.com/ponycloud/celly/',
+    packages=['celly'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: System :: Distributed Computing',
+        'License :: OSI Approved :: MIT License',
+    ],
+    scripts=[]
+)
+
+
+# vim:set sw=4 ts=4 et:
